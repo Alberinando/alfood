d10a0761048d2a0e6ab8a0efb139fc166367d055
Alberinando <80666910+Alberinando@users.noreply.github.com>
2023-06-27 11:57:31 -0300

    Exclução e edição de restaurantes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './page/Home';
 import VitrineRestaurantes from './page/VitrineRestaurantes';
-import AdministracaoRestaurantes from './page/AdministracaoRestaurantes/administração/AdministracaoRestaurantes';
-import FormularioRestaurante from './page/AdministracaoRestaurantes/administração/FormularioRestaurante';
+import AdministracaoRestaurantes from './page/AdministracaoRestaurantes/administracao/AdministracaoRestaurantes';
+import FormularioRestaurante from './page/AdministracaoRestaurantes/administracao/FormularioRestaurante';
 
 function App() {
 
@@ -12,6 +12,7 @@ function App() {
       <Route path="/restaurantes" element={<VitrineRestaurantes />} />
       <Route path="/admin/restaurantes" element={<AdministracaoRestaurantes />} />
       <Route path="/admin/restaurantes/novo" element={<FormularioRestaurante />} />
+      <Route path="/admin/restaurantes/:id" element={<FormularioRestaurante />} />
     </Routes>
   );
 }
---
+++ src/page/AdministracaoRestaurantes/administracao/AdministracaoRestaurantes.tsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+import IRestaurante from "../../../interfaces/IRestaurante"
+import { TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody, Button } from "@mui/material"
+import { Link } from "react-router-dom"
+
+const AdministracaoRestaurantes = () => { 
+    const [restaurantes, setrestaurantes] = useState<IRestaurante[]>([])
+    useEffect(() => {
+        axios.get<IRestaurante[]>('http://localhost:8000/api/v2/restaurantes/')
+            .then(resposta => setrestaurantes(resposta.data))
+    }, [])
+    const excluir = (restauranteAhSerExcluido: IRestaurante) => {
+        axios.delete(`http://localhost:8000/api/v2/restaurantes/${restauranteAhSerExcluido.id}/`)
+            .then(() => {
+                const listaRestaurante = restaurantes.filter(restaurante => restaurante.id !== restauranteAhSerExcluido.id)
+                setrestaurantes([...listaRestaurante])
+            })
+    }
+    return (
+        <TableContainer component={Paper}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>
+                            Nome
+                        </TableCell>
+                        <TableCell>
+                            Editar
+                        </TableCell>
+                        <TableCell>
+                            Excluir
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {restaurantes.map(restaurante => <TableRow key={restaurante.id}>
+                        <TableCell>
+                            {restaurante.nome}
+                        </TableCell>
+                        <TableCell>
+                            [ <Link to={`/admin/restaurantes/${restaurante.id}`}>editar</Link> ]
+                        </TableCell>
+                        <TableCell>
+                            <Button variant="outlined" color="error" onClick={() => excluir(restaurante)}>
+                                Excluir
+                            </Button>
+                        </TableCell>
+                    </TableRow>)}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    )
+}
+
+export default AdministracaoRestaurantes
